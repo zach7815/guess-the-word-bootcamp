c962cf0f2b6d569950c6fbaedcb564f76ecae90f
zach7815 <53745970+zach7815@users.noreply.github.com>
2023-07-30 22:01:57 +0800

    Updates

--- src/utils.js
+++
@@ -1,6 +0,0 @@
-import words from "./words.json";
-
-export const getRandomWord = () => {
-  // Lowercase words for simplicity
-  return words[Math.floor(Math.random() * words.length)].toLowerCase();
-};
